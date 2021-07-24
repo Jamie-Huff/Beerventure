82cfa5ee02c853126a4a1ce2384e0a64d33c06ba
Xiaowei <xiaowei.c.li@gmail.com>
2021-07-24 16:46:40 +0000

    add CREATE TABLE code

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -1,7 +1,10 @@
--- Drop and recreate Users table (Example)
+-- DROP TABLE IF EXISTS users CASCADE;
 
-DROP TABLE IF EXISTS users CASCADE;
+--USERS
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(225) NOT NULL,
+  password VARCHAR(225) NOT NULL,
+  phone_number VARCHAR(20)
 );
