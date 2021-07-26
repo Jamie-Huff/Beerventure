0e42feb78d0a34bef56e64a36536742f1e377a7d
jamiehuff737@gmail.com <jamiehuff737@gmail.com>
2021-07-26 17:10:18 +0000

    ajax to load search request - notcompletely working

---
+++ routes/vendors.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    let query = `SELECT * FROM vendors`;
+    return db.query(query)
+      .then(data => {
+        res.render("urls_search")
+        const items = data.rows;
+        return res.json({ items });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+  });
+
+  router.post("/", (req, res) => {
+    console.log(req.body)
+    const city = req.body.city
+    let query = `SELECT name, city FROM vendors WHERE $1 = city`
+    console.log(city)
+    return db.query(query, [city])
+      .then(data => {
+        const items = data.rows;
+        console.log(items)
+        return res.json({ items });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
+    })
+  return router;
+};
+
--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use(express.static("public"));
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
 const homepage = require("./routes/userRoutes");
-const search = require("./routes/search");
+const search = require("./routes/vendors");
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
