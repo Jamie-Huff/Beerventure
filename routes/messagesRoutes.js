f8776331caadf1671c6e3cb53f86069c6d54f9f2
Xiaowei <xiaowei.c.li@gmail.com>
2021-07-27 15:25:11 +0000

    committing because computer is lagging

--- routes/messagesRoutes.js
+++ routes/messagesRoutes.js
@@ -7,44 +7,20 @@
 
 const express = require('express');
 const router  = express.Router();
-const { getMessages } = require('./database')
 
 module.exports = (db) => {
   console.log(db)
 
-  // router.get("/", (req, res) => {
-
-  //   getMessages()
-  //     .then(results => {
-  //       const templateVars = {
-  //         messages: results
-  //       }
-  //       return res.render("../views/urls_messages", {templateVars} )
-  //     })
-  //     .catch(err => {
-  //       res
-  //         .status(500)
-  //         .json({ error: err.message });
-  //     });
-  // });
-
-  // router.post()
-
-
   router.get("/", (req, res) => {
-    db.query(`
+    return db.query(`
     SELECT messages.*, vendors.name as name
     FROM messages
     JOIN vendors ON vendor_id = vendors.id
     ORDER BY vendor_id;
     `)
     .then(data => {
-      const users = data.rows;
-      res.json({ users });
-      const templateVars = {
-        messages: users[0]
-      }
-      return res.render("../views/urls_messages", { templateVars })
+      const messages = data.rows;
+      res.render("../views/urls_messages", { messages });
     })
     .catch(err => {
       res
@@ -53,6 +29,17 @@ module.exports = (db) => {
     });
   });
 
+  router.post("/", (req, res) => {
+    db.query(`
+    SELECT messages.*, vendors.name as name
+    FROM messages
+    JOIN vendors ON vendor_id = vendors.id
+    ORDER BY vendor_id;
+    `)
+    .then((queryResults) => {
+      res.json(queryResults.rows[0])
+    })
+  });
 
   return router;
 };
