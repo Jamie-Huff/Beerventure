49a698dacdf18759818a8da5c87067047edfe3b6
Xiaowei <xiaowei.c.li@gmail.com>
2021-07-27 14:20:24 +0000

    no real changes

--- server.js
+++ server.js
@@ -53,6 +53,7 @@ app.use("/api/users", usersRoutes(db));
 app.use("/", featuredItems(db));
 app.use("/", homepage(db));
 app.use("/search", search(db));
+app.use("/api/messages", messages(db));
 // Note: mount other resources here, using the same pattern above
 
 // app.use('/', (req, res, next) => {//app.use works for EVERYTHING (get, post)
