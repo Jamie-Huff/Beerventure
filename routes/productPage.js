f75f5b40c9b546c2e4148086222cb5cf9cd61de9
jamiehuff737@gmail.com <jamiehuff737@gmail.com>
2021-07-28 19:32:50 +0000

    nav bar renders differently depending on the users login state

--- routes/productPage.js
+++ routes/productPage.js
@@ -3,17 +3,30 @@ const router  = express.Router();
 
 module.exports = (db) => {
  router.get(`/:productURL`, (req, res) => {
-    userId = req.session.user
+    const user = req.session.user
     let value = req.params.productURL.split('&')
+    console.log(user)
     const productName = value[0].substr(1, value[0].length)
     const itemId = value[1].substr(7)
     const query = (`SELECT *
     FROM items
     WHERE id = $1
     ;`)
+
     return db.query(query, [itemId])
     .then(value => {
-      let templateVars = {userId, items: value.rows}
+      let templateVars = {}
+      if (!user) {
+        templateVars = {
+          userObject: null,
+          items: value.rows
+        }
+      } else {
+        templateVars = {
+          userObject: user,
+          items: value.rows
+        }
+      }
       console.log(templateVars)
       res.render('urls_product', templateVars)
     })
