99dee4c7ff791318779c7a1e6b33b0095c0ee76a
jamiehuff737@gmail.com <jamiehuff737@gmail.com>
2021-07-29 18:49:00 +0000

    ability to filter search results by price

--- routes/vendors.js
+++ routes/vendors.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 const express = require('express');
 const router  = express.Router();
 
@@ -12,12 +13,14 @@ module.exports = (db) => {
         if (user) {
           templateVars = {
             items,
-            userObject: user
+            userObject: user,
+            city: null
           }
         } else {
           templateVars = {
           items,
-          userObject: null
+          userObject: null,
+          city: null
           }
         }
 
@@ -31,9 +34,9 @@ module.exports = (db) => {
       });
   });
 
-  router.post("/", (req, res) => {
+  router.get("/searchByCity", (req, res) => {
     let user = req.session.user
-    const city = req.body.city
+    const city = req.query.city
     let query = `
     SELECT price, items.name, category, vendors.city as city, date_posted, image, vendors.name as vendor
     FROM items
@@ -47,12 +50,14 @@ module.exports = (db) => {
         if (user) {
           templateVars = {
             items,
-            userObject: user
+            userObject: user,
+            city,
           }
         } else {
           templateVars = {
           items,
-          userObject: null
+          userObject: null,
+          city,
           }
         }
 
@@ -64,6 +69,84 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
     })
+
+
+    router.get("/lowToHigh", (req, res) => {
+      console.log(req.query)
+      let user = req.session.user
+      const city = req.query.city
+      let query = `
+      SELECT price, items.name, category, vendors.city as city, date_posted, image, vendors.name as vendor
+      FROM items
+      JOIN vendors ON vendors.id = vendor_id
+      WHERE city = $1
+      ORDER BY price;
+      `
+      return db.query(query, [city])
+        .then(data => {
+          let templateVars;
+          const items = data.rows;
+          if (user) {
+            templateVars = {
+              items,
+              userObject: user,
+              city,
+            }
+          } else {
+            templateVars = {
+            items,
+            userObject: null,
+            city,
+            }
+          }
+
+          res.render('urls_search', templateVars)
+        })
+        .catch(err => {
+          res
+            .status(500)
+            .json({ error: err.message });
+        });
+      })
+
+      router.get("/highToLow", (req, res) => {
+        console.log(req.query)
+        let user = req.session.user
+        const city = req.query.city
+        let query = `
+        SELECT price, items.name, category, vendors.city as city, date_posted, image, vendors.name as vendor
+        FROM items
+        JOIN vendors ON vendors.id = vendor_id
+        WHERE city = $1
+        ORDER BY price DESC;
+        `
+        return db.query(query, [city])
+          .then(data => {
+            let templateVars;
+            const items = data.rows;
+            if (user) {
+              templateVars = {
+                items,
+                userObject: user,
+                city,
+              }
+            } else {
+              templateVars = {
+              items,
+              userObject: null,
+              city,
+              }
+            }
+
+            res.render('urls_search', templateVars)
+          })
+          .catch(err => {
+            res
+              .status(500)
+              .json({ error: err.message });
+          });
+        })
+
   return router;
 };
 
